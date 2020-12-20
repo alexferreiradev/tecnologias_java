a34d6ab75560c39b1b6806b6410c01187e61bb64
Alex Rabelo Ferreira <arf92@live.com>
2020-12-19 23:44:58 -0300

    add: codigos de testes para validações

--- JPA/exemplo-jpa1.0/src/main/java/dev/alexferreira/produto/GreetingResource.java
+++ JPA/exemplo-jpa1.0/src/main/java/dev/alexferreira/produto/GreetingResource.java
@@ -1,24 +1,53 @@
 package dev.alexferreira.produto;
 
-import dev.alexferreira.produto.ProdutoService;
+import dev.alexferreira.produto.model.Item;
+import dev.alexferreira.produto.model.Produto;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Path("/produto")
 public class GreetingResource {
 
-    @Inject
-    ProdutoService produtoService;
+	@Inject
+	ProdutoService produtoService;
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        Produto produto = produtoService.saveProduto();
-        Produto produto1 = produtoService.getProduto(produto.getId());
-        return "Hello Quarkus com Elder Moraes: " + produto.getDescricao();
-    }
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		Produto produto = produtoService.saveProduto();
+		Produto produto1 = produtoService.getProduto(produto.getId());
+		String descricao;
+		if (produto1.getItemList() == null) {
+			List<Item> itemList = produtoService.getItemPorProduto(produto.getId());
+			descricao = itemList.stream().map(new Function<Item, String>() {
+				@Override
+				public String apply(Item item) {
+					return item.getDescricao() + ":fromGet";
+				}
+			}).collect(Collectors.joining(","));
+		} else {
+			descricao = produto1.getItemList().get(0).getDescricao();
+		}
+		return "Hello Quarkus com Elder Moraes: " + produto1.getId() + ":" + produto.getDescricao() + " desc Item: " + descricao;
+//		return "Hello Quarkus com Elder Moraes: " + produto1.getDescricao() + " desc Item: ";
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String get2(@PathParam("id") Integer id) {
+		Produto produto = produtoService.getOtherSession(id);
+		List<Item> itemList = produto.getItemList();
+		if (itemList == null) return "item null";
+		Item item = itemList.get(0);
+		return "produto de outra sessao: " + produto.getId() + ":" + produto.getDescricao() + ":" + item.getId();
+	}
 }
--- JPA/exemplo-jpa1.0/src/main/java/dev/alexferreira/produto/ProdutoService.java
+++ JPA/exemplo-jpa1.0/src/main/java/dev/alexferreira/produto/ProdutoService.java
@@ -1,30 +1,81 @@
 package dev.alexferreira.produto;
 
+import dev.alexferreira.produto.model.Item;
+import dev.alexferreira.produto.model.Produto;
+import org.hibernate.Criteria;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.ejb.HibernateEntityManagerFactory;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.List;
 
-@ApplicationScoped
+@RequestScoped
 public class ProdutoService {
 
 	@Inject
 	EntityManager entityManager;
 
+	/**
+	 * @deprecated Por que eu quero
+	 *
+	 * @param id
+	 * @return
+	 *
+	 */
+	@Deprecated
 	public Produto getProduto(Integer id) {
-		Query query = entityManager.createQuery("select p from Produto p");
-		List resultList = query.getResultList();
-		if (resultList.isEmpty()) return null;
-		return (Produto) resultList.get(0);
+//		entityManager.clear();
+//		Query query = entityManager.createQuery("select p from Produto p join fetch p.itemList where p.id = :id");
+//		Query query = entityManager.createQuery("select p from Produto p where p.id = :id");
+//		query.setParameter("id", id);
+//		Produto produto = (Produto) query.getSingleResult();
+//		Produto produto = entityManager.find(Produto.class, id);
+//		Session session = entityManager.unwrap(Session.class);
+//		Criteria criteria = session.createCriteria(Produto.class);
+//		List list = criteria.list();
+		Produto produto = entityManager.getReference(Produto.class, id);
+		return produto;
+//		if (resultList.isEmpty()) return null;
+//		return (Produto) resultList.get(0);
 	}
 
-	@Transactional
 	public Produto saveProduto() {
+		entityManager.getTransaction().begin();
+		Item item = new Item();
 		Produto produto = new Produto();
-		produto.setDescricao("Teste de desc");
-		entityManager.persist(produto);
+		item.setDescricao("Teste de desc2");
+		produto.setDescricao("Teste de desc2");
+//		entityManager.persist(produto);
+//		entityManager.flush();
+//		produto.setItemList(Collections.singletonList(item));
+		item.setProduto(produto);
+		entityManager.persist(item);
+//		entityManager.refresh(produto);
+		entityManager.getTransaction().commit();
+
 		return produto;
+	}
+
+	public List<Item> getItemPorProduto(Integer id) {
+		Query query = entityManager.createQuery("select i from Item i where i.produto.id = :id");
+		query.setParameter("id", id);
+		return query.getResultList();
+	}
+
+	public Produto getOtherSession(Integer id) {
+//		Query query = entityManager.createQuery("select p from Produto p join fetch p.itemList where p.id = :id");
+//		Query query = entityManager.createQuery("select p from Produto p where p.id = :id");
+//		query.setParameter("id", id);
+//		Object produto = query.getSingleResult();
+		Produto produto = entityManager.getReference(Produto.class, id);
+//		Produto produto = entityManager.find(Produto.class, id);
+		return (Produto) produto;
 	}
 }
