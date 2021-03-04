1f99c94a8ab4b5002658ba2bbdc116d435bce18f
Alex Rabelo Ferreira <arf92@live.com>
2021-03-04 07:39:54 -0300

    update: teste para junit 5

--- java_11/projeto_basico/src/test/java/dev/alexferreira/conteudo/datatype/FloatTest.java
+++ java_11/projeto_basico/src/test/java/dev/alexferreira/conteudo/datatype/FloatTest.java
@@ -1,8 +1,9 @@
 package dev.alexferreira.conteudo.datatype;
 
-import org.junit.Assert;
-import org.junit.Test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class FloatTest {
 
@@ -10,15 +11,17 @@ public class FloatTest {
 	public void wrapper() {
 		float numero = 2.5f;
 
-		Assert.assertEquals((Float)2.5f, Float.valueOf(numero));
+		Assertions.assertEquals((Float)2.5f, Float.valueOf(numero));
 	}
 
+	@DisplayName("Valida a promocao de tipos como char para int")
 	@Test
 	public void typePromotion() {
 		float f = 2.5f;
 		short s = 25;
 		int i = 10;
+		char c = 'A';
 
-		Assert.assertEquals(10, f+ s+ i, 1);
+		Assertions.assertEquals(102.5, f+ s+ i+ c, 1);
 	}
 }
