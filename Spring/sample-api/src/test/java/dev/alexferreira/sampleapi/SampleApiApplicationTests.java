e542f22c0de2bdb251f60e4d13bfb2785e93a042
Alex Rabelo Ferreira <arf92@live.com>
2021-10-21 18:52:44 -0300

    #7 add: sample project to api with MVC
    - project setup with MVC + gradle created from spring init website

---
+++ Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/SampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexferreira.sampleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
