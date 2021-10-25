247ca64cbe496be34e5ad4ed708f0d943e474099
Alex Rabelo Ferreira <arf92@live.com>
2021-10-25 15:11:15 -0300

    add: logger Bean
    - add bean to use injection in components

---
+++ Spring/sample-api/src/main/java/dev/alexferreira/sampleapi/core/LoggerBeanFactory.java
@@ -0,0 +1,24 @@
+package dev.alexferreira.sampleapi.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class LoggerBeanFactory {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggerBeanFactory.class);
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Logger createLogger(InjectionPoint injectionPoint) {
+		Logger logger = LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+		LOGGER.trace("Log injection configured to {}", injectionPoint.getMember().getDeclaringClass().getName());
+
+		return logger;
+	}
+}
