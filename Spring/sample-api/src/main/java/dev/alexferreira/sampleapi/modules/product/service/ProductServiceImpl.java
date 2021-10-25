85daaebe6ec03a84f871aa069d8b556c7cf6d49a
Alex Rabelo Ferreira <arf92@live.com>
2021-10-25 18:10:38 -0300

    update: service to use custom repository
    - use of custom repository to custom queries
    - use of transaction to save data

--- Spring/sample-api/src/main/java/dev/alexferreira/sampleapi/modules/product/service/ProductServiceImpl.java
+++ Spring/sample-api/src/main/java/dev/alexferreira/sampleapi/modules/product/service/ProductServiceImpl.java
@@ -3,34 +3,39 @@ package dev.alexferreira.sampleapi.modules.product.service;
 import dev.alexferreira.sampleapi.modules.product.dto.ProdutoDTO;
 import dev.alexferreira.sampleapi.modules.product.dto.mapper.ProductMapper;
 import dev.alexferreira.sampleapi.modules.product.model.Product;
+import dev.alexferreira.sampleapi.modules.product.repository.CustomProductRepository;
 import dev.alexferreira.sampleapi.modules.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-	private ProductRepository repository;
-	private ProductMapper mapper;
+	private final ProductRepository repository;
+	private final CustomProductRepository customProductRepository;
+	private final ProductMapper mapper;
 
 	@Autowired
-	public ProductServiceImpl(ProductRepository repository, ProductMapper mapper) {
+	public ProductServiceImpl(ProductRepository repository, CustomProductRepository customProductRepository, ProductMapper mapper) {
 		this.repository = repository;
+		this.customProductRepository = customProductRepository;
 		this.mapper = mapper;
 	}
 
 	@Override
 	public ProdutoDTO getProduto() {
-		return mapper.toDTO(repository.getProduct());
+		return mapper.toDTO(customProductRepository.getFirstProduct());
 	}
 
+	@Transactional
 	@Override
 	public void saveProduct(ProdutoDTO dto) {
 		validateOrThrow(dto);
 		Product product = mapper.toEntity(dto);
-		repository.saveProduct(product);
+		repository.save(product);
 	}
 
 	private void validateOrThrow(ProdutoDTO dto) {
