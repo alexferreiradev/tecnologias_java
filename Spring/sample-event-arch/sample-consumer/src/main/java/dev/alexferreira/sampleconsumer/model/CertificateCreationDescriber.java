5c8e8e8168ad2a7ca2b041b789382284320e663d
Alex-Ferreira <alex.ferreira@invillia.com>
2021-11-05 09:17:49 -0300

    update: models with methods to conversion
    - add method to convert data in certificate model
    
    - certificate models was copied from external project. So, we convert our models to the project instead of refactor the services copied.

--- Spring/sample-event-arch/sample-consumer/src/main/java/dev/alexferreira/sampleconsumer/model/CertificateCreationDescriber.java
+++ Spring/sample-event-arch/sample-consumer/src/main/java/dev/alexferreira/sampleconsumer/model/CertificateCreationDescriber.java
@@ -1,5 +1,7 @@
 package dev.alexferreira.sampleconsumer.model;
 
+import dev.alexferreira.sampleconsumer.module.certificate.model.Participant;
+
 public class CertificateCreationDescriber {
     public User user;
     public EventDescriber eventDescriber;
@@ -10,5 +12,15 @@ public class CertificateCreationDescriber {
     public CertificateCreationDescriber(User user, EventDescriber eventDescriber) {
         this.user = user;
         this.eventDescriber = eventDescriber;
+    }
+
+    public Participant createParticipant() {
+        Participant participant = new Participant();
+        participant.setName(user.name);
+        participant.setCpf(user.identifier);
+        participant.setEvent(eventDescriber.createEvent());
+        participant.setHour("4h");
+
+        return participant;
     }
 }
--- Spring/sample-event-arch/sample-consumer/src/main/java/dev/alexferreira/sampleconsumer/model/EventDescriber.java
+++ Spring/sample-event-arch/sample-consumer/src/main/java/dev/alexferreira/sampleconsumer/model/EventDescriber.java
@@ -1,9 +1,22 @@
 package dev.alexferreira.sampleconsumer.model;
 
+import dev.alexferreira.sampleconsumer.module.certificate.model.Event;
+
+import java.util.Date;
 import java.util.List;
 
 public class EventDescriber {
     public String name;
     public String data;
     public List<User> participants;
+
+    public Event createEvent() {
+        Event event = new Event();
+        event.setName(name);
+        event.setExecutor(name);
+        event.setDateStarted(new Date());
+        event.setTalkerTopics("não informado");
+
+        return event;
+    }
 }
