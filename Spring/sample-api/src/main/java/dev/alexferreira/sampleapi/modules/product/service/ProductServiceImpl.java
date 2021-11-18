26ce475ddf5cc4333d41b0c3b63b620d3239fb7b
Alex-Ferreira <alex.ferreira@invillia.com>
2021-11-18 17:53:40 -0300

    #25 - update: ProductService with mongodb
    - add method to list all logs sort by data
    - add use of log from mongo to store logs when product was created

--- Spring/sample-api/src/main/java/dev/alexferreira/sampleapi/modules/product/service/ProductServiceImpl.java
+++ Spring/sample-api/src/main/java/dev/alexferreira/sampleapi/modules/product/service/ProductServiceImpl.java
@@ -3,12 +3,16 @@ package dev.alexferreira.sampleapi.modules.product.service;
 import dev.alexferreira.sampleapi.modules.product.dto.ProdutoDTO;
 import dev.alexferreira.sampleapi.modules.product.dto.mapper.ProductMapper;
 import dev.alexferreira.sampleapi.modules.product.model.Product;
+import dev.alexferreira.sampleapi.modules.product.model.ProdutoLog;
 import dev.alexferreira.sampleapi.modules.product.repository.CustomProductRepository;
 import dev.alexferreira.sampleapi.modules.product.repository.ProductRepository;
+import dev.alexferreira.sampleapi.modules.product.repository.ProdutoLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,12 +21,14 @@ public class ProductServiceImpl implements ProductService {
 	private final ProductRepository repository;
 	private final CustomProductRepository customProductRepository;
 	private final ProductMapper mapper;
+	private final ProdutoLogRepository logRepository;
 
 	@Autowired
-	public ProductServiceImpl(ProductRepository repository, CustomProductRepository customProductRepository, ProductMapper mapper) {
+	public ProductServiceImpl(ProductRepository repository, CustomProductRepository customProductRepository, ProductMapper mapper, ProdutoLogRepository logRepository) {
 		this.repository = repository;
 		this.customProductRepository = customProductRepository;
 		this.mapper = mapper;
+		this.logRepository = logRepository;
 	}
 
 	@Override
@@ -36,6 +42,13 @@ public class ProductServiceImpl implements ProductService {
 		validateOrThrow(dto);
 		Product product = mapper.toEntity(dto);
 		repository.save(product);
+
+		logRepository.insert(ProdutoLog.createFrom(this, String.format("salvo produto: %s", product.getName())));
+	}
+
+	@Override
+	public List<ProdutoLog> getAllLogs() {
+		return logRepository.findAll(Sort.by(Sort.Direction.DESC, "data"));
 	}
 
 	private void validateOrThrow(ProdutoDTO dto) {
