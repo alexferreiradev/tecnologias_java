80a9ce13240db218c05f7d3ae5675f5a7ae45474
Alex-Ferreira <alex.ferreira@invillia.com>
2021-11-18 17:53:40 -0300

    #25 - update: ProductService with mongodb
    - add method to list all logs sort by data
    - add use of log from mongo to store logs when product was created

--- src/main/java/dev/alexferreira/sampleapi/modules/product/service/ProductService.java
+++ src/main/java/dev/alexferreira/sampleapi/modules/product/service/ProductService.java
@@ -1,10 +1,15 @@
 package dev.alexferreira.sampleapi.modules.product.service;
 
 import dev.alexferreira.sampleapi.modules.product.dto.ProdutoDTO;
+import dev.alexferreira.sampleapi.modules.product.model.ProdutoLog;
+
+import java.util.List;
 
 public interface ProductService {
 
 	ProdutoDTO getProduto();
 
 	void saveProduct(ProdutoDTO dto);
+
+	List<ProdutoLog> getAllLogs();
 }
