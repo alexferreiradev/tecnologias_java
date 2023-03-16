94a125d61d98ca09bf3fd6bae3f7cdab113e1b5e
Alex Rabelo Ferreira <arf92@live.com>
2023-03-15 21:32:26 -0300

    add: integration test
    - add application integration test

---
+++ Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/SampleApiApplicationTest.java
@@ -0,0 +1,16 @@
+package dev.alexferreira.sampleapi;
+
+import dev.alexferreira.sampleapi.testdefault.*;
+import org.junit.jupiter.api.*;
+import org.springframework.test.util.*;
+
+class SampleApiApplicationTest extends IntegrationTest {
+
+   @Test void main() {
+      // todo add test some property
+      // todo add test some dependency use, like db
+      // todo add test some configuration from spring
+      // todo add docker configuration
+      AssertionErrors.fail("not imp");
+   }
+}
---
+++ Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/testdefault/IntegrationTest.java
@@ -0,0 +1,8 @@
+package dev.alexferreira.sampleapi.testdefault;
+
+import org.springframework.boot.test.context.*;
+import org.springframework.test.context.*;
+
+@SpringBootTest @ActiveProfiles("test") public abstract class IntegrationTest {
+
+}
