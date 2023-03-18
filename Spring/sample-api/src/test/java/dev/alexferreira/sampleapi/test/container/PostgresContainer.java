d094beaccc92f1291e7187fab9fc7d439b3688f4
Alex Rabelo Ferreira <arf92@live.com>
2023-03-17 23:10:53 -0300

    add: postgress container for tests
    - add wrapper postgres container

--- Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/test/container/PostgresContainer.java
+++ Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/test/container/PostgresContainer.java
@@ -1,14 +1,25 @@
 package dev.alexferreira.sampleapi.test.container;
 
 import dev.alexferreira.sampleapi.test.spring.*;
+import org.testcontainers.containers.*;
+import org.testcontainers.utility.*;
 
 import java.util.*;
 
-public class PostgresContainer implements Initializer.Configurator {
+public class PostgresContainer extends PostgreSQLContainer<PostgresContainer> implements Initializer.Configurator {
+
+   public PostgresContainer() {
+      super(DockerImageName.parse("postgres"));
+
+      withUsername("postgres");
+      withPassword("postgres");
+      withDatabaseName("sample_api");
+
+      start();
+   }
 
    @Override
    public Map<String, String> createDynamicProperties() {
-      System.out.println("Creating property");
-      return Collections.emptyMap();
+      return Map.of("spring.datasource.url", getJdbcUrl());
    }
 }
