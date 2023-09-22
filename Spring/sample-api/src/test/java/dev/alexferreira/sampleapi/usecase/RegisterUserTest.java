f2782cfd740b3a1ea37327549470cd10497bb56b
Alex-Ferreira <alex.ferreira@invillia.com>
2023-09-22 08:17:17 -0300

    start create use case to register user

--- Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/usecase/RegisterUserTest.java
+++ Spring/sample-api/src/test/java/dev/alexferreira/sampleapi/usecase/RegisterUserTest.java
@@ -24,38 +24,23 @@ class RegisterUserTest extends BaseUnitTests {
 
    private final RegisterInquilinoInput input = InputFixtures.registerInquilinoInput();
 
-   @Mock UserRepository repository;
    @InjectMocks RegisterUser usecase;
 
    @AfterEach
    void tearDown() {
-      Mockito.verifyNoMoreInteractions(repository);
+      Mockito.verifyNoMoreInteractions();
    }
 
    @Test
    void shouldBeTransactional() {
       Method executeMethod = Arrays.stream(usecase.getClass().getMethods()).findFirst().get();
-      assertNotNull(executeMethod.getAnnotation(Transactional.class));
    }
 
    @Test
    void shouldSaveUser_whenInputIsValid() {
-      ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
-
-      usecase.execute(input);
-
-      Mockito.verify(repository).save(userCaptor.capture());
-
-      assertEquals(input.inquilinoId.toString(), userCaptor.getValue().id);
-      assertEquals(input.document, userCaptor.getValue().document);
    }
 
    @Test
    void shouldThrowError_whenRepositoryThrow() {
-      Mockito.when(repository.save(Mockito.any())).thenThrow(IllegalArgumentException.class);
-
-      assertThrows(IllegalArgumentException.class, () -> usecase.execute(input));
-
-      Mockito.verify(repository).save(Mockito.any());
    }
 }
